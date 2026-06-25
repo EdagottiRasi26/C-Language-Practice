266fdbd381bc36d39de7f6643d3cdcc56ecb0649
EdagottiRasi26 <rasiedagotti12345@gmail.com>
2026-06-25 13:30:08 +0530

    1stfile

---
+++ level1/1st.c
@@ -0,0 +1,13 @@
+// Get a number from user and add 2 to that number and print the result.
+
+#include <stdio.h>
+int main(){
+    int result,num;
+    printf("Get the number from the user:");
+    scanf("%d",&num);
+    result=num+2;
+    printf("Result after adding 2 to the number is: %d",result);
+    return 0;
+}
+
+
