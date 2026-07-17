2cbf66f291332536573f5fb2c2b9c4fb415566ac
EdagottiRasi26 <rasiedagotti12345@gmail.com>
2026-07-17 16:00:41 +0530

    5th file

---
+++ level2/5th.c
@@ -0,0 +1,12 @@
+/* Write a loop program to print odd numbers 1 to 9. */
+/*op:3 5 7 9 */
+
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=1;i<=9;i++){
+      if(i%2!=0){
+          printf("%d\n",i);
+      } 
+    }
+}
