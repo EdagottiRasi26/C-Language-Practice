673c29b68786f254490e91941a4d155af0cd8760
EdagottiRasi26 <rasiedagotti12345@gmail.com>
2026-07-17 16:05:48 +0530

    6th file

---
+++ level2/6th.c
@@ -0,0 +1,11 @@
+// Write a loop program to print the two-digit odd numbers, below 20.
+
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=10;i<=20;i++){
+        if(i%2!=0){
+            printf("2 digit odd numbers below is :%d\n",i);
+        }
+    }
+}
