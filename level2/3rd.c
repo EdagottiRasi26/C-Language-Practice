9faa4d7652b6f85796cece72a71b5a13d62b1244
EdagottiRasi26 <rasiedagotti12345@gmail.com>
2026-07-17 14:37:33 +0530

    3rd file

---
+++ level2/3rd.c
@@ -0,0 +1,10 @@
+// Write a loop program to print sum of 1 to 5.
+
+#include<stdio.h>
+int main(){
+    int i,sum=0;
+    for(i=1;i<=5;i++){
+        sum = sum + i;
+    }
+        printf("sum:%d",sum);
+}
