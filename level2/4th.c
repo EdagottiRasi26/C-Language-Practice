44f7fa27d32933bea91544fe1df0aef30eef0de4
EdagottiRasi26 <rasiedagotti12345@gmail.com>
2026-07-17 15:54:24 +0530

    4th file

---
+++ level2/4th.c
@@ -0,0 +1,13 @@
+/*  Write a loop program to print sum of 6 to 1. 
+op: 21*/
+
+#include<stdio.h>
+int main(){
+ int i,sum=0;
+ for(i=6;i>=1;i--){
+     sum=sum+i;
+ }
+     printf("sum=%d",sum);
+}
+
+
